a9045d48dfe7fe996106f9c4081b7e9665cda6d4
damienbod <damien_bod@hotmail.com>
2019-03-08 07:57:02 +0100

    removing the IHttpContextAccessor, no longer required

--- Secure_gRpc/Secure_gRpc.Server/Services/GreeterService.cs
+++ Secure_gRpc/Secure_gRpc.Server/Services/GreeterService.cs
@@ -1,35 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Greet;
 using Grpc.Core;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 
 namespace Secure_gRpc
 {
     [Authorize(Policy = "protectedScope")]
     public class GreeterService : Greeter.GreeterBase
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
-        public GreeterService(IHttpContextAccessor httpContextAccessor)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
-
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
-            // Manual authorization as temp workaround
-            var httpContext = _httpContextAccessor.HttpContext;
-            var authOk = httpContext.User.HasClaim(claim => claim.Type == "scope" && claim.Value == "grpc_protected_scope");
-            if (!authOk)
-            {
-                // not needed if the auth middleware is working
-            }
-
             return Task.FromResult(new HelloReply
             {
                 Message = "Hello " + request.Name
