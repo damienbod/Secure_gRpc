5dd736f82ddefde43c7799dbd169bf2ec1d7177f
damienbod <damien_bod@hotmail.com>
2019-08-16 10:11:21 +0200

    code clean up

--- SecureGrpc.Server/Startup.cs
+++ SecureGrpc.Server/Startup.cs
@@ -39,7 +39,6 @@ namespace SecureGrpc.Server
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
