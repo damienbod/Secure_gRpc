fba025a0e5523726dfa222959933be5e8e4d7e07
damienbod <damien_bod@hotmail.com>
2019-12-16 15:45:03 +0100

    fix middleware call order

--- SecureTokenService/Startup.cs
+++ SecureTokenService/Startup.cs
@@ -232,11 +232,9 @@ namespace StsServerIdentity
                 }
             });
 
-            app.UseIdentityServer();
-
             app.UseRouting();
 
-            app.UseAuthentication();
+            app.UseIdentityServer();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
